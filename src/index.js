ba46ae7b29030e9ceeeb92b254867034a5e021dd
nqobilencube <165543847+nqobilencube@users.noreply.github.com>
2024-11-10 10:05:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "396262bbd3oce4f20c3b41t44bae4cfa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#Mandy-Weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
